c7d4121f71299e4f439ac45f59ec0e4504f4db69
show0386 <51075438+show0386@users.noreply.github.com>
2021-05-10 20:52:01 +0900

    Add files via upload

---
+++ SSIM.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import cv2
+import torch
+import numpy
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+img1 = cv2.imread(r'C:\Users\KIM\Desktop\Untitled.png')
+img2 = cv2.imread(r'C:\Users\KIM\Desktop\Untitled2.png')
+
+res_img1 = cv2.resize(img1, (256,256))
+res_img2 = cv2.resize(img2, (256,256))
+
+ssim = tf.image.ssim(res_img1, res_img2, max_val=255, filter_size=11,
+                          filter_sigma=1.5, k1=0.01, k2=0.03)
+
+plt.imshow(img1,cmap="gray"), plt.axis("off")
+plt.show()
+
+plt.imshow(img2,cmap="gray"), plt.axis("off")
+plt.show()
+
+print(type(ssim.numpy()))
+print("ACCCRACY SSIM %.4f" % ssim.numpy())
+
